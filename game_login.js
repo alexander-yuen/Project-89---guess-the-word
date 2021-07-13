b88f901ec67ea09a7a96af591a97ebee8416af9b
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-07-12 20:53:50 -0400

    get from the localStorage

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function addUser() {
+    p1 = document.getElementById("player1_name_input").value;
+    p2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1", p1);
+    localStorage.setItem("player2", p2);
+
+    window.location = "game_page.html";
+}
